5d84da46ffef27ab34ad049e61fd5c3f0ede8760
Deven Agrawal <devenagrawal.810@gmail.com>
2018-10-21 17:55:22 -0700

    All parts including both the extra credit assignments

--- main-scene.js
+++ main-scene.js
@@ -21,7 +21,7 @@ class Cube extends Shape                 // Here's a complete, working example o
         // It stinks to manage arrays this big.  Later we'll show code that generates these same cube vertices more automatically.
       }
   }
-
+window.flag = true
 window.Transforms_Sandbox = window.classes.Transforms_Sandbox =
 class Transforms_Sandbox extends Tutorial_Animation   // This subclass of some other Scene overrides the display() function.  By only
 { display( graphics_state )                           // exposing that one function, which draws everything, this creates a very small code
@@ -41,7 +41,7 @@ class Transforms_Sandbox extends Tutorial_Animation   // This subclass of some o
 
       const t = this.t = graphics_state.animation_time/1000;     // Find how much time has passed in seconds, and use that to place shapes.
 
-      model_transform = model_transform.times( Mat4.translation([ 0, -2, 0 ]) );  // Tweak our coordinate system downward for the next shape.
+      /* model_transform = model_transform.times( Mat4.translation([ 0, -2, 0 ]) ).times(Mat4.scale([1, 1, 1]));  // Tweak our coordinate system downward for the next shape.
       this.shapes.ball.draw( graphics_state, model_transform, this.plastic.override({ color: blue }) );    // Draw the ball.
 
       if( !this.hover )     // The first line below won't execute if the button on the page has been toggled:
@@ -49,7 +49,16 @@ class Transforms_Sandbox extends Tutorial_Animation   // This subclass of some o
       model_transform   = model_transform.times( Mat4.rotation( 1, Vec.of( 0,0,1 ) ) )  // Rotate another axis by a constant value.
                                          .times( Mat4.scale      ([ 1,   2, 1 ]) )      // Stretch the coordinate frame.
                                          .times( Mat4.translation([ 0,-1.5, 0 ]) );     // Translate down enough for the two volumes to miss.
-      this.shapes.box.draw( graphics_state, model_transform, this.plastic.override({ color: yellow }) );   // Draw the bottom box.
+
+       this.shapes.box.draw( graphics_state, model_transform, this.plastic.override({ color: blue }) );   // Draw the bottom box.
+       model_transform = model_transform.times(Mat4.scale([3,3,3]));
+       this.shapes.tetrahedron.draw( graphics_state, model_transform, this.plastic.override({ color: blue }) ); */
+       function switchup(i) {if (i%2==0) { return {color:blue} } else { return {color:yellow}}} 
+       for(let i = 0; i < 5; i++) {
+         model_transform = model_transform.times(Mat4.translation([0, -2, 0]))
+         this.shapes.box.draw(graphics_state, model_transform, this.plastic.override(switchup(i)))
+//          console.log(i)
+       }
     }
 }
 
@@ -62,6 +71,10 @@ class Cube_Outline extends Shape
                                 // When a set of lines is used in graphics, you should think of the list entries as
                                 // broken down into pairs; each pair of vertices will be drawn as a line segment.
 
+        this.positions.push( ...Vec.cast( [-1,-1,-1], [1,-1,-1], [-1,-1,1], [1,-1,1], [1,1,-1],  [-1,1,-1],  [1,1,1],  [-1,1,1],
+                                          [-1,-1,-1], [-1,-1,1], [-1,1,-1], [-1,1,1], [1,-1,1],  [1,-1,-1],  [1,1,1],  [1,1,-1],
+                                          [-1,-1,-1],  [-1,1,-1],  [-1,-1,1], [-1,1,1], [1,-1,1],  [1,1,1], [1,-1,-1], [1,1,-1] ) ); 
+        this.colors.push( ...Array(24).fill(Color.of(1,1,1,1)));
         this.indexed = false;       // Do this so we won't need to define "this.indices".
       }
   }
@@ -71,7 +84,19 @@ class Cube_Single_Strip extends Shape
   { constructor()
       { super( "positions", "normals" );
 
-        // TODO (Extra credit part I)
+        /* this.positions.push( ...Vec.cast( [-1,-1,-1], [1,-1,-1], [1,1,-1], [-1,1,-1], [-1,1,1],  [-1,1,1],  [1,1,1],  [1,-1,1],
+                                          [-1,-1,1], [-1,1,1], [-1,-1,-1], [-1,1,-1], [1,-1,-1],  [1,1,-1],  [1,1,1]) );
+                                                  this.normals.push(   ...Vec.cast( [-1,-1,-1], [1,-1,-1], [1,1,-1], [-1,1,-1], [-1,1,1],  [-1,1,1],  [1,1,1],  [1,-1,1],
+                                          [-1,-1,1], [-1,1,1], [-1,-1,-1], [-1,1,-1], [1,-1,-1],  [1,1,-1],  [1,1,1]) );
+        this.indices.push( 0,1,2, 0,2,3, 2,3,4, 3,4,2, 2,4,5, 4,5,6, 4,6,7,
+                           7,8,9, 8,9,10, 7,9,6, 6,9,11, 6,11,12, 6,12, 13);      */
+        
+        this.positions.push( ...Vec.cast( [-1,-1,-1], [1,-1,-1], [1,1,-1], [-1,1,-1], [-1,-1,1], [1,-1,1], [1,1,1], [-1,1,1]));
+        this.normals.push( ...Vec.cast( [-1,-1,-1], [1,-1,-1], [1,1,-1], [-1,1,-1], [-1,-1,1], [1,-1,1], [1,1,1], [-1,1,1]));
+        this.indices.push(0,1,2, 0,2,3, 0,3,7, 0,4,7, 4,7,5, 5,7,6, 5,6,1, 1,6,2, 0,1,4, 1,4,5, 2,3,7, 2,7,6);
+                 // Those two lists, positions and normals, fully describe the "vertices".  What's the "i"th vertex?  Simply the combined
+                 // data you get if you look up index "i" of both lists above -- a position and a normal vector, together.  Now let's
+                 // tell it how to connect vertex entries into triangles.  Every three indices in this list makes one triangle:
       }
   }
 
@@ -101,34 +126,60 @@ class Assignment_One_Scene extends Scene_Component
 
         this.lights = [ new Light( Vec.of( 0,5,5,1 ), Color.of( 1, .4, 1, 1 ), 100000 ) ];
 
+        this.shouldSway = true
+        this.showOutline = false
+        this.scaleFactor = 1;
+        this.newBoxes = 7;
+        this.boxColors = [];
         this.set_colors();
       }
     set_colors() {
-          // TODO:  Create a class member variable to store your cube's colors.
+          for(let i = 0; i<this.newBoxes; i++) {
+            this.boxColors[i] = Color.of(Math.random(),Math.random(),Math.random(),1)
+          }
       }
     make_control_panel()             // Draw the scene's buttons, setup their actions and keyboard shortcuts, and monitor live measurements.
       { this.key_triggered_button( "Change Colors", [ "c" ], this.set_colors );    // Add a button for controlling the scene.
         this.key_triggered_button( "Outline",       [ "o" ], () => {
-
-          // TODO:  Requirement 5b:  Set a flag here that will toggle your outline on and off
+          this.showOutline = !this.showOutline
           } );
         this.key_triggered_button( "Sit still",     [ "m" ], () => {
-
-          // TODO:  Requirement 3d:  Set a flag here that will toggle your swaying motion on and off.
+          this.shouldSway = !this.shouldSway
           } );
       }
-    draw_box( graphics_state, model_transform )
-      {
-        // TODO:  Helper function for requirement 3 (see hint).
-        //        This should make changes to the model_transform matrix, draw the next box, and return the newest model_transform.
 
+      swayingMotion(t, sway) {
+          if(sway) {
+            return .02*Math.PI + .02*Math.PI*Math.sin(3*2*Math.PI*t)
+          }
+          else {
+            return .04*Math.PI
+          }
+        }
+        
+    draw_box( graphics_state, model_transform, showOutline, i, angle)
+      {
+        
+        model_transform = model_transform.times(Mat4.translation([1, 1, 0]))
+                                         .times(Mat4.rotation(this.swayingMotion(angle, this.shouldSway), Vec.of(0, 0, -1)))
+                                         .times(Mat4.translation([-1, 1, 0]))
+        if(showOutline) {
+          this.shapes.outline.draw(graphics_state, model_transform, this.white, "LINES")
+        }
+        else {
+          this.shapes.box.draw(graphics_state, model_transform, this.plastic.override({color:this.boxColors[i]}))
+        }
         return model_transform;
       }
     display( graphics_state )
       { graphics_state.lights = this.lights;        // Use the lights stored in this.lights.
-
         let model_transform = Mat4.identity();
+        const t = this.t = graphics_state.animation_time/1000;     // Find how much time has passed in seconds, and use that to place shapes.
 
-        // TODO:  Draw your entire scene here.  Use this.draw_box( graphics_state, model_transform ) to call your helper.
+        model_transform = model_transform.times(Mat4.scale([1, this.scaleFactor, 1])) // This is where I've implemented EC2 - just change the member variable scaleFactor to see the effects
+        this.shapes.strip.draw(graphics_state, model_transform, this.plastic.override({color:Color.of(0,1,1,1)}), "TRIANGLE_STRIP"); //This is the call to the structure defined in EC1. 
+        for(let i=0; i<this.newBoxes; i++) {
+            model_transform = this.draw_box(graphics_state, model_transform, this.showOutline, i, t)
+        } 
       }
   }
